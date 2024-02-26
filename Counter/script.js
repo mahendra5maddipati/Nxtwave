986a5afe418ad0bfca01a351575e075e6c422c6e
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-02-26 22:27:37 +0530

    added eventListeners and queryListeners original eventListeners

--- Counter/script.js
+++ Counter/script.js
@@ -1,33 +1,40 @@
-let counterElement = document.getElementById("counterValue");
+document.addEventListener("DOMContentLoaded", function() {
 
-function onIncrement() {
-    let previousCounterValue = counterElement.textContent;
-    let updatedCounterValue = parseInt(previousCounterValue) + 1;
-    if (updatedCounterValue > 0) {
-        counterElement.style.color = "green";
-    } else if (updatedCounterValue < 0) {
-        counterElement.style.color = "red";
-    } else {
+    let counterElement = document.getElementById("counterValue");
+    
+    document.querySelector(".decrement").addEventListener("click", onDecrement);
+    document.querySelector(".reset").addEventListener("click", onReset);
+    document.querySelector(".increment").addEventListener("click", onIncrement);
+
+    function onIncrement() {
+        let previousCounterValue = counterElement.textContent;
+        let updatedCounterValue = parseInt(previousCounterValue) + 1;
+        if (updatedCounterValue > 0) {
+            counterElement.style.color = "green";
+        } else if (updatedCounterValue < 0) {
+            counterElement.style.color = "red";
+        } else {
+            counterElement.style.color = "black";
+        }
+        counterElement.textContent = updatedCounterValue;
+    }
+
+    function onDecrement() {
+        let previousCounterValue = counterElement.textContent;
+        let updatedCounterValue = parseInt(previousCounterValue) - 1;
+        if (updatedCounterValue > 0) {
+            counterElement.style.color = "green";
+        } else if (updatedCounterValue < 0) {
+            counterElement.style.color = "red";
+        } else {
+            counterElement.style.color = "black";
+        }
+        counterElement.textContent = updatedCounterValue;
+    }
+
+    function onReset() {
+        let counterValue = 0;
+        counterElement.textContent = counterValue;
         counterElement.style.color = "black";
     }
-    counterElement.textContent = updatedCounterValue;
-}
-
-function onDecrement() {
-    let previousCounterValue = counterElement.textContent;
-    let updatedCounterValue = parseInt(previousCounterValue) - 1;
-    if (updatedCounterValue > 0) {
-        counterElement.style.color = "green";
-    } else if (updatedCounterValue < 0) {
-        counterElement.style.color = "red";
-    } else {
-        counterElement.style.color = "black";
-    }
-    counterElement.textContent = updatedCounterValue;
-}
-
-function onReset() {
-    let counterValue = 0;
-    counterElement.textContent = counterValue;
-    counterElement.style.color = "black";
-}
+});
