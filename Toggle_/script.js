d53d90fd7fbbc9a902d1fd82c17de9df7a876b9d
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-01 22:30:56 +0530

    Script file added.

---
+++ Toggle_/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let puppyImage = document.getElementById("puppyImage");
+    let likeIcon = document.getElementById("likeIcon");
+    let likeButton = document.getElementById("likeButton");
+    let imageLiked = false;
+
+    function onClickLikeButton() {
+        if (imageLiked === false) {
+            puppyImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-liked-img.png";
+            likeIcon.style.color = "#0967d2";
+            likeButton.style.backgroundColor = "#0967d2";
+            likeButton.style.color = "#ffffff";
+            imageLiked = true;
+        } else {
+            puppyImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png";
+            likeIcon.style.color = "#cbd2d9";
+            likeButton.style.backgroundColor = "#cbd2d9";
+            likeButton.style.color = "#9aa5b1";
+            imageLiked = false;
+        }
+    }
+    likeButton.addEventListener("click", onClickLikeButton);
+
+});
