7d5e617dffac78742218bf33b507e6e4c5d1d859
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-04 22:55:26 +0530

    Added event listener to the buttons.

--- Addition_Game/script.js
+++ Addition_Game/script.js
@@ -1,29 +1,33 @@
-let first = document.getElementById("firstNumber");
-let second = document.getElementById("secondNumber");
-let userInput = document.getElementById("userInput");
-let result = document.getElementById("gameResult");
+document.addEventListener("DOMContentLoaded", function() {
+    let first = document.getElementById("firstNumber");
+    let second = document.getElementById("secondNumber");
+    let userInput = document.getElementById("userInput");
+    let result = document.getElementById("gameResult");
 
-function restart(){
-    first.textContent = Math.ceil(Math.random() * 100);
-    second.textContent = Math.ceil(Math.random() * 100);
-    result.textContent = "";
-    userInput.value = "";
-}
-
-restart();
-
-function check(){
-    let firstRandom = parseInt(first.textContent);
-    let secondRandom = parseInt(second.textContent);
-    let userSum = parseInt(userInput.value);
-    
-    let sum = firstRandom + secondRandom;
-    
-    if (userSum === sum){
-        result.textContent = "Congratulations! You got it right.";
-        result.style.backgroundColor = "#028a0f";
-    } else {
-        result.textContent = "Please Try Again!";
-        result.style.backgroundColor = "#1e217c";
+    function restart(){
+        first.textContent = Math.ceil(Math.random() * 100);
+        second.textContent = Math.ceil(Math.random() * 100);
+        result.textContent = "";
+        userInput.value = "";
     }
-}
+
+    restart();
+
+    function check(){
+        let firstRandom = parseInt(first.textContent);
+        let secondRandom = parseInt(second.textContent);
+        let userSum = parseInt(userInput.value);
+        
+        let sum = firstRandom + secondRandom;
+        
+        if (userSum === sum){
+            result.textContent = "Congratulations! You got it right.";
+            result.style.backgroundColor = "#028a0f";
+        } else {
+            result.textContent = "Please Try Again!";
+            result.style.backgroundColor = "#1e217c";
+        }
+    }
+    checkButton.addEventListener("click", check);
+    restartButton.addEventListener("click", restart);
+});
