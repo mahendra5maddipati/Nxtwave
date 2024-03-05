1926390ba4a867a6617169916fb5eef12767b366
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-05 21:35:35 +0530

    last commit added event listener.

--- Random_Color_Picker/script.js
+++ Random_Color_Picker/script.js
@@ -1,28 +1,31 @@
-let bgColorsArray = [];
-for (let r = 0; r <= 255; r += 51) {
-    for (let g = 0; g <= 255; g += 51) {
-        for (let b = 0; b <= 255; b += 51) {
-            const rgbColor = `rgb(${r},${g},${b})`;
-            bgColorsArray.push(rgbColor);
+document.addEventListener("DOMContentLoaded", function() {
+    let bgColorsArray = [];
+    for (let r = 0; r <= 255; r += 51) {
+        for (let g = 0; g <= 255; g += 51) {
+            for (let b = 0; b <= 255; b += 51) {
+                const rgbColor = `rgb(${r},${g},${b})`;
+                bgColorsArray.push(rgbColor);
+            }
         }
     }
-}
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
     }
-}
 
-shuffleArray(bgColorsArray);
+    shuffleArray(bgColorsArray);
 
-let button = document.getElementById("button");
-let bg = document.getElementById("bgContainer");
-let currentIndex = 0;
+    let button = document.getElementById("button");
+    let bg = document.getElementById("bgContainer");
+    let currentIndex = 0;
 
-function clickMe() {
-    let currentColor = bgColorsArray[currentIndex];
-    bg.style.backgroundColor = currentColor;
-    currentIndex = (currentIndex + 1) % bgColorsArray.length;
-}
+    function clickMe() {
+        let currentColor = bgColorsArray[currentIndex];
+        bg.style.backgroundColor = currentColor;
+        currentIndex = (currentIndex + 1) % bgColorsArray.length;
+    }
+    button.addEventListener("click", clickMe);
+});
