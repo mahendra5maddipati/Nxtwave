f20c10235a2f968a7cd4bce1e68b15b8e35ce4ab
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-07 22:44:15 +0530

    Added event listener function.

--- Selecting_Tabs/script.js
+++ Selecting_Tabs/script.js
@@ -1,39 +1,44 @@
-let aboutB = document.getElementById("aboutButton");
-let visitB = document.getElementById("timeToVisitButton");
-let attractionsB = document.getElementById("attractionsButton");
-let aboutT = document.getElementById("aboutTab");
-let visitT = document.getElementById("timeToVisitTab");
-let attractionsT = document.getElementById("attractionsTab");
+document.addEventListener("DOMContentLoaded", function() {
+    let aboutB = document.getElementById("aboutButton");
+    let visitB = document.getElementById("timeToVisitButton");
+    let attractionsB = document.getElementById("attractionsButton");
+    let aboutT = document.getElementById("aboutTab");
+    let visitT = document.getElementById("timeToVisitTab");
+    let attractionsT = document.getElementById("attractionsTab");
 
-visitT.classList.add("d-none");
-attractionsT.classList.add("d-none");
-
-function about() {
-    aboutT.classList.remove("d-none");
     visitT.classList.add("d-none");
     attractionsT.classList.add("d-none");
 
-    aboutB.classList.add("selected-button");
-    visitB.classList.remove("selected-button");
-    attractionsB.classList.remove("selected-button");
-}
+    function about() {
+        aboutT.classList.remove("d-none");
+        visitT.classList.add("d-none");
+        attractionsT.classList.add("d-none");
 
-function visit() {
-    aboutT.classList.add("d-none");
-    visitT.classList.remove("d-none");
-    attractionsT.classList.add("d-none");
+        aboutB.classList.add("selected-button");
+        visitB.classList.remove("selected-button");
+        attractionsB.classList.remove("selected-button");
+    }
 
-    aboutB.classList.remove("selected-button");
-    visitB.classList.add("selected-button");
-    attractionsB.classList.remove("selected-button");
-}
+    function visit() {
+        aboutT.classList.add("d-none");
+        visitT.classList.remove("d-none");
+        attractionsT.classList.add("d-none");
 
-function attractions() {
-    aboutT.classList.add("d-none");
-    visitT.classList.add("d-none");
-    attractionsT.classList.remove("d-none");
+        aboutB.classList.remove("selected-button");
+        visitB.classList.add("selected-button");
+        attractionsB.classList.remove("selected-button");
+    }
 
-    aboutB.classList.remove("selected-button");
-    visitB.classList.remove("selected-button");
-    attractionsB.classList.add("selected-button");
-}
+    function attractions() {
+        aboutT.classList.add("d-none");
+        visitT.classList.add("d-none");
+        attractionsT.classList.remove("d-none");
+
+        aboutB.classList.remove("selected-button");
+        visitB.classList.remove("selected-button");
+        attractionsB.classList.add("selected-button");
+    }
+    aboutB.addEventListener("click", about);
+    visitB.addEventListener("click", visit);
+    attractionsB.addEventListener("click", attractions);
+});
