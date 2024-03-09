49cf25df8038c1bd8276db9e08db19f67a39b1ee
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-09 22:23:31 +0530

    added event listener to the class instance

--- Simple_User_Profile/script.js
+++ Simple_User_Profile/script.js
@@ -1,22 +1,24 @@
-let profileDetails = {
-    imgSrc: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/user-profile-img.png",
-    name: "Mahendra Maddipati",
-    role: "Software Engineer"
-};
+document.addEventListener("DOMContentLoaded", function () {
+    let profileDetails = {
+        imgSrc: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/user-profile-img.png",
+        name: "Mahendra Maddipati",
+        role: "Software Engineer"
+    };
 
-let img1 = document.getElementById("imgContainer");
-let container = document.getElementById("profileContainer");
+    let img1 = document.getElementById("imgContainer");
+    let container = document.getElementById("profileContainer");
 
-container.classList.add("d-flex", "flex-column", "justify-content-center", "text-center");
+    container.classList.add("d-flex", "flex-column", "justify-content-center", "text-center");
 
-let img = document.createElement("img");
-img.setAttribute("src", profileDetails.imgSrc);
-img1.appendChild(img);
+    let img = document.createElement("img");
+    img.setAttribute("src", profileDetails.imgSrc);
+    img1.appendChild(img);
 
-let heading = document.createElement("h1");
-heading.textContent = profileDetails.name;
-container.appendChild(heading);
+    let heading = document.createElement("h1");
+    heading.textContent = profileDetails.name;
+    container.appendChild(heading);
 
-let para = document.createElement("p");
-para.textContent = "Role:" + profileDetails.role;
-container.appendChild(para);
+    let para = document.createElement("p");
+    para.textContent = "Role:" + profileDetails.role;
+    container.appendChild(para);
+});
