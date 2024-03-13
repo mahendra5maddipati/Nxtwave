67bd907451e4224142ecdd9322f003366a1789a0
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-13 10:41:29 +0530

    added event listener

--- Deleting_Ordered_Items/script.js
+++ Deleting_Ordered_Items/script.js
@@ -1,32 +1,36 @@
-let itemList = [{
-    itemName: "Veg Biryani",
-    uniqueNo: 1,
-},
-{
-    itemName: "Chicken 65",
-    uniqueNo: 2,
-},
-{
-    itemName: "Paratha",
-    uniqueNo: 3,
-}
-];
+document.addEventListener("DOMContentLoaded", function() {
 
-function removeItem(uniqueNo) {
-let orderedItemList = document.getElementById("orderedItemList");
-let itemToRemove = document.getElementById(`item${uniqueNo}`);
-orderedItemList.removeChild(itemToRemove);
-}
-itemList.forEach(item => {
-let orderedItemList = document.getElementById("orderedItemList");
-let listItem = document.createElement("li");
-listItem.id = `item${item.uniqueNo}`;
-listItem.textContent = item.itemName;
-let cancelButton = document.createElement("button");
-cancelButton.id = `button${item.uniqueNo}`;
-cancelButton.classList.add("btn", "btn-danger", "ml-3");
-cancelButton.textContent = "Cancel";
-cancelButton.addEventListener("click", () => removeItem(item.uniqueNo));
-listItem.appendChild(cancelButton);
-orderedItemList.appendChild(listItem);
+    let itemList = [{
+        itemName: "Veg Biryani",
+        uniqueNo: 1,
+    },
+    {
+        itemName: "Chicken 65",
+        uniqueNo: 2,
+    },
+    {
+        itemName: "Paratha",
+        uniqueNo: 3,
+    }
+    ];
+
+    function removeItem(uniqueNo) {
+    let orderedItemList = document.getElementById("orderedItemList");
+    let itemToRemove = document.getElementById(`item${uniqueNo}`);
+    orderedItemList.removeChild(itemToRemove);
+    }
+    itemList.forEach(item => {
+    let orderedItemList = document.getElementById("orderedItemList");
+    let listItem = document.createElement("li");
+    listItem.id = `item${item.uniqueNo}`;
+    listItem.textContent = item.itemName;
+    let cancelButton = document.createElement("button");
+    cancelButton.id = `button${item.uniqueNo}`;
+    cancelButton.classList.add("btn", "btn-danger", "ml-3");
+    cancelButton.textContent = "Cancel";
+    cancelButton.addEventListener("click", () => removeItem(item.uniqueNo));
+    listItem.appendChild(cancelButton);
+    orderedItemList.appendChild(listItem);
+    });
+    
 });
