95927bda62db010d1e376b2f1f93feeaa0c4ce49
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-17 21:55:55 +0530

    script file

--- Simple_Splice_Playground/script.js
+++ Simple_Splice_Playground/script.js
@@ -1,20 +1,24 @@
-let arr = [1, 7, 3, 1, 0, 20, 77];
+document.addEventListener("DOMContentLoaded", function(){
+    let arr = [1, 7, 3, 1, 0, 20, 77];
 
-let stringifyArray = JSON.stringify(arr);
+    let stringifyArray = JSON.stringify(arr);
 
-let updatedArray = document.getElementById("updatedArray");
+    let updatedArray = document.getElementById("updatedArray");
 
-updatedArray.textContent = stringifyArray;
-
-let start = document.getElementById("startIndexInput");
-let del = document.getElementById("deleteCountInput");
-let add = document.getElementById("itemToAddInput");
-
-function splice() {
-    let a = start.value;
-    let b = del.value;
-    let c = add.value;
-    arr.splice(a, b, c);
-    stringifyArray = JSON.stringify(arr);
     updatedArray.textContent = stringifyArray;
-}
+
+    let start = document.getElementById("startIndexInput");
+    let del = document.getElementById("deleteCountInput");
+    let add = document.getElementById("itemToAddInput");
+
+    function splice() {
+        let a = start.value;
+        let b = del.value;
+        let c = add.value;
+        arr.splice(a, b, c);
+        stringifyArray = JSON.stringify(arr);
+        updatedArray.textContent = stringifyArray;
+    }
+    let spliceBtn = document.getElementById("spliceBtn");
+    spliceBtn.addEventListener("click", splice);
+});
