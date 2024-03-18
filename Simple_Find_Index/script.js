417ff6e23277764402ea968efb780511880c5043
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-18 22:12:00 +0530

    dom is creted

--- Simple_Find_Index/script.js
+++ Simple_Find_Index/script.js
@@ -1,17 +1,19 @@
-let numbers = [17, 31, 77, 20, 63];
+document.addEventListener("DOMContentLoaded", function(){
+    let numbers = [17, 31, 77, 20, 63];
 
-let input = document.getElementById("userInput");
-let button = document.getElementById("findBtn");
-let index = document.getElementById("indexOfNumber");
+    let input = document.getElementById("userInput");
+    let button = document.getElementById("findBtn");
+    let index = document.getElementById("indexOfNumber");
 
-function findIndexOfNumber() {
-    let a = parseInt(input.value);
-    let item = numbers.findIndex(function(eachitem) {
-        if (eachitem === a) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-    index.textContent = item;
-}
+    function findIndexOfNumber() {
+        let a = parseInt(input.value);
+        let item = numbers.findIndex(function(eachitem) {
+            if (eachitem === a) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        index.textContent = item;
+    }
+});
