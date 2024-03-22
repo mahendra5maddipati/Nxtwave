47e96aaf60686464540eee15a51c198e567ca27e
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-22 13:52:40 +0530

    script file

---
+++ Simple_Custom_Range_Counter/script.js
@@ -0,0 +1,25 @@
+let from = document.getElementById("fromUserInput");
+let to = document.getElementById("toUserInput");
+let counter = document.getElementById("counterText");
+let button = document.getElementById("startBtn");
+button.onclick = function() {
+    if (from.value === "") {
+        alert("Enter the from value");
+    } else if (to.value === "") {
+        alert("Enter the to value");
+    } else {
+        let a = parseInt(from.value);
+        let b = parseInt(to.value);
+        counter.textContent = a;
+
+        let uniqueId = setInterval(function() {
+            a = a + 1;
+
+            if (a > b) {
+                clearInterval(uniqueId);
+            } else {
+                counter.textContent = a;
+            }
+        }, 1000);
+    }
+};
