43b0c16441c858d71b37d70cdb11d30c8de5250d
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-24 12:12:53 +0530

    inline even listener

--- Simple_Peace_Maker/script.js
+++ Simple_Peace_Maker/script.js
@@ -1,41 +1,42 @@
-let twenty = document.getElementById("twentySecondsBtn");
-let thirty = document.getElementById("thirtySecondsBtn");
-let forty = document.getElementById("fortySecondsBtn");
-let oneMinute = document.getElementById("oneMinuteBtn");
-let text = document.getElementById("timerText");
-let counter;
+document.addEventListener("DOMContentLoaded", function(){
+    let twenty = document.getElementById("twentySecondsBtn");
+    let thirty = document.getElementById("thirtySecondsBtn");
+    let forty = document.getElementById("fortySecondsBtn");
+    let oneMinute = document.getElementById("oneMinuteBtn");
+    let text = document.getElementById("timerText");
+    let counter;
 
 
-function startTimer() {
-    text.textContent = counter + " seconds left";
-    let id = setInterval(function() {
+    function startTimer() {
         text.textContent = counter + " seconds left";
-        counter = counter - 1;
-        if (counter === -1) {
-            text.textContent = "Your moment is complete";
-            clearInterval(id);
-        }
-    }, 1000);
-}
-twenty.onclick = function() {
-    counter = 20;
-    startTimer();
-};
+        let id = setInterval(function() {
+            text.textContent = counter + " seconds left";
+            counter = counter - 1;
+            if (counter === -1) {
+                text.textContent = "Your moment is complete";
+                clearInterval(id);
+            }
+        }, 1000);
+    }
+    twenty.onclick = function() {
+        counter = 20;
+        startTimer();
+    };
 
-thirty.onclick = function() {
-    counter = 30;
-    startTimer();
-};
+    thirty.onclick = function() {
+        counter = 30;
+        startTimer();
+    };
 
-forty.onclick = function() {
-    counter = 40;
-    startTimer();
-};
+    forty.onclick = function() {
+        counter = 40;
+        startTimer();
+    };
 
-oneMinute.onclick = function() {
-    counter = 60;
-    startTimer();
-};
+    oneMinute.onclick = function() {
+        counter = 60;
+        startTimer();
+    };
 });
 
 // twenty.onclick = function(){
