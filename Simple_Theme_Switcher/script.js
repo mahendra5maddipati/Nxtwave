9235f4334e71a3dfb06f451a65055807a86c5e86
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-25 22:16:37 +0530

    added event listener

--- Simple_Theme_Switcher/script.js
+++ Simple_Theme_Switcher/script.js
@@ -1,17 +1,19 @@
-function changeTheme(event) {
-    if (event.key === 'Enter') {
-        let container = document.getElementById("bgContainer");
-        let heading = document.getElementById("heading");
-        let input = document.getElementById("themeUserInput");
-        if (input.value === 'Light') {
-            container.style.backgroundImage = "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-light-bg.png')";
-            heading.style.color = '#014d40';
-        } else if (input.value === 'Dark') {
-            container.style.backgroundImage = "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-dark-bg.png')";
-            heading.style.color = 'White';
-        } else {
-            alert('Enter the valid theme');
+document.addEventListener("DOMContentLoaded", function(){
+    function changeTheme(event) {
+        if (event.key === 'Enter') {
+            let container = document.getElementById("bgContainer");
+            let heading = document.getElementById("heading");
+            let input = document.getElementById("themeUserInput");
+            if (input.value === 'Light') {
+                container.style.backgroundImage = "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-light-bg.png')";
+                heading.style.color = '#014d40';
+            } else if (input.value === 'Dark') {
+                container.style.backgroundImage = "url('https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/change-theme-dark-bg.png')";
+                heading.style.color = 'White';
+            } else {
+                alert('Enter the valid theme');
+            }
+            input.value = '';
         }
-        input.value = '';
     }
-}
+});
