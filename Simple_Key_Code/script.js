7f474110adb8d4ec1fee5c84ece43c1f0b442b17
mahendra maddipati <mahendra5maddipati@gmail.com>
2024-03-26 22:19:25 +0530

    DOMContentLoaded

--- Simple_Key_Code/script.js
+++ Simple_Key_Code/script.js
@@ -1,8 +1,10 @@
-let input = document.getElementById("userInput");
-let keys = document.getElementById("keyCodeList");
+document.addEventListener("DOMContentLoaded", function(){
+    let input = document.getElementById("userInput");
+    let keys = document.getElementById("keyCodeList");
 
-input.addEventListener('keydown', function(event) {
-    let li = document.createElement("li");
-    li.textContent = event.keyCode;
-    keys.appendChild(li);
+    input.addEventListener('keydown', function(event) {
+        let li = document.createElement("li");
+        li.textContent = event.keyCode;
+        keys.appendChild(li);
+    });
 });
